45f4652c839374980adefb9373d566a7daf8d835
IsuJay <isujayawardana1220@gmail.com>
2020-07-27 22:25:30 +0530

    add animations

--- js.js
+++ js.js
@@ -12,6 +12,20 @@ function animationForm(){
             if(input.type === "text" && validateUser(input)){
                 nextSlide(parent, nextForm);
             }
+            else if (input.type === "email" && validateEmail(input)){
+                nextSlide(parent, nextForm);
+            }
+            else if (input.type === "password" && validateUser(input)){
+                nextSlide(parent, nextForm);
+            }
+            else{
+                parent.style.animation = "shake 0.5s ease" ;
+            }
+                
+            parent.addEventListener("animationend", () => {
+                parent.style.animation = "" ;
+            });
+
         });
     });
 }
@@ -27,7 +41,16 @@ function validateUser(user){
     }
 }
 
-functionvalidateEmail()
+function validateEmail(email){
+    const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(validation.test(email.value)){
+        error("rgb(65, 174, 182)");
+        return true;
+    }
+    else{
+        error("rgb(189,87,87)");
+    }
+}
 
 function nextSlide(parent, nextForm){
     parent.classList.add('innactive');
