b382c2e5202fc73c4c7673087c54c6e6d1238519
IsuJay <isujayawardana1220@gmail.com>
2020-07-27 21:55:53 +0530

    add some new items

--- js.js
+++ js.js
@@ -1,5 +1,42 @@
 function animationForm(){
-    
+
+    const arrows = document.querySelectorAll(".fa-arrow-down");
+
+    arrows.forEach( arrow => {
+        arrow.addEventListener("click", () => {
+            console.log("Hello");
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+
+            if(input.type === "text" && validateUser(input)){
+                nextSlide(parent, nextForm);
+            }
+        });
+    });
 }
 
-animationForm();
+function validateUser(user){
+    if( user.value.length < 6 ){
+        console.log("not enough characters");
+        error("rgb(189,87,87)");
+    }
+    else{
+        error("rgb(65, 174, 182)");
+        return true;
+    }
+}
+
+functionvalidateEmail()
+
+function nextSlide(parent, nextForm){
+    parent.classList.add('innactive');
+    parent.classList.remove('active');
+    nextForm.classList.add('active');
+}
+
+function error(color){
+    document.body.style.backgroundColor = color;
+}
+
+animationForm(); 
